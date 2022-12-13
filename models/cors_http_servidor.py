6285771f2e9b820a9ed5f669f01f04a53ad28569
REV09 <58372035+REV09@users.noreply.github.com>
2022-12-13 17:20:12 -0600

    Add files via upload

---
+++ models/cors_http_servidor.py
@@ -0,0 +1,6 @@
+from http.server import SimpleHTTPRequestHandler
+
+class CORSRequestHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        SimpleHTTPRequestHandler.end_headers(self)
